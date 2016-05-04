178ae2d5075778c04c0dd11183b1fc4c3f34951a
brothersong <1010888575@qq.com>
2016-05-05 00:46:06 +0800

    Signed-off-by: brothersong <1010888575@qq.com>

---
+++ secondGroup/teamLu/teamLu/LoginWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace teamLu
+{
+    /// <summary>
+    /// loginWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void LoginCheck(object sender, RoutedEventArgs e)
+        {
+            string user = userTxt.Text;
+            string pwd = pwdTxt.Password;
+            if(user.Length == 0 || pwd.Length == 0)
+            {
+                MessageBox.Show("请输入用户名和密码！", "提示");
+                LogHelper.WriteLogInfo(typeof(LoginWindow),"没有输入用户名和密码");
+                return;
+            }
+            bool passCheck = Validation.IsExistedUser(user, pwd);
+            if (passCheck)
+            {
+                MainWindow win = new MainWindow();
+                win.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("用户名或密码错误，请重试！", "提示");
+            }
+        }
+
+        
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            Register register = new Register();
+
+            register.ShowDialog();
+        }
+    }
+}
---
+++ secondGroup/teamLu/teamLu/Validation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace teamLu
+{
+    public class Validation
+    {
+        public static bool IsExistedUser(string user, string pwd)
+        {
+            bool isExisted = false;
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader("user.txt");
+            }
+            catch (Exception e)
+            {
+                LogHelper.WriteLogErr(typeof(Validation), "没有找到用户信息文件！");
+                return false;
+            }
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] info = s.Split('#');
+                if (info[0].Equals(user) && info[1].Equals(pwd))
+                {
+                    isExisted = true;
+                    break;
+                }
+                else
+                    continue;
+            }
+            sr.Close();
+            if (isExisted)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
